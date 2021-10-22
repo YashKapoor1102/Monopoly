09ab9cf2a0a168deafcfa25cd82711ffd0f4f09c
yashk <yashkapoor1102@gmail.com>
2021-10-21 21:12:47 -0400

    Added important methods in GameUI

---
+++ GameUI.java
@@ -0,0 +1,27 @@
+public class GameUI {
+
+    public GameUI() {
+
+    }
+
+    public void displayPlayerInput() {
+
+    }
+
+    public void displayBoard() {
+
+    }
+
+    public void displayCommands(Player player, boolean hasRolled) {
+
+    }
+
+    public void displayDiceRoll(String player, int roll) {
+
+    }
+
+    public void displayPayment(String payer, String payee, int payment)  {
+
+    }
+
+}
