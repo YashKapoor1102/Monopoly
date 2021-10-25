1ef16363b5e1a56d9742daa0a9051a36e067c488
Oliverlu07 <92889612+Oliverlu07@users.noreply.github.com>
2021-10-24 21:50:17 -0400

    Add files via upload

---
+++ GameLogic.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class GameLogic extends Gameboard {
+
+    private ArrayList<Player> players = new ArrayList<>();
+
+
+    private Gameboard gameboard = new Gameboard();
+
+    private GameUI gameUI = new GameUI();
+
+    public void GameLogic(Gameboard gameboard){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
